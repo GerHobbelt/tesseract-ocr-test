57ff598badf88ae9d27a8f208be3e750ff69c0d6
Ger Hobbelt <ger@hobbelt.com>
2023-10-13 00:50:46 +0200

    ARGV-parameterize the basicAPItest and add fixes for monolithic build mode.

--- testing/basicapitest.cpp
+++ testing/basicapitest.cpp
@@ -1,33 +1,59 @@
 #include <leptonica/allheaders.h>
-#include <stdio.h>
 #include <tesseract/baseapi.h>
 
+#if defined(BUILD_MONOLITHIC)
+#include "monolithic_tests.h"
+#endif
+
+#include <stdio.h>
+
+
+static bool fileExists(const char *path) {
+	FILE *f = fopen(path, "rb");
+	if (!f)
+		return false;
+	fclose(f);
+	return true;
+}
 
 
 #if defined(BUILD_MONOLITHIC)
 #  define main tesseract_basicAPI_test_main
+#  define TESTFILE_PATHPREFIX  "../../thirdparty/tesseract/test/"
+#else
+#  define TESTFILE_PATHPREFIX  ""
 #endif
 
-int main(void) {
+int main(int argc, const char **argv) {
+	const char *testfilepath = argv[1];
+	if (!testfilepath || !*testfilepath) {
+		testfilepath = TESTFILE_PATHPREFIX "../test/testing/phototest.tif";
+	}
+	if (!fileExists(testfilepath)) {
+		fprintf(stderr, "Could not access test file \"%s\".\n", testfilepath);
+		return 1;
+	}
+
     tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
     // Initialize tesseract-ocr with English
     if (api->InitSimple("../../tessdata", "eng")) {
         fprintf(stderr, "Could not initialize tesseract.\n");
-        exit(1);
+        return 1;
     }
 
     // Open input image with leptonica library
-    Pix *image = pixRead("../test/testing/phototest.tif");
+    Pix *image = pixRead(testfilepath);
     api->SetImage(image);
     // Get OCR result
     char *outText = api->GetUTF8Text();
     printf("OCR output:\n%s", outText);
 
     // Destroy used object and release memory
-    api->End();
+	delete [] outText;
+	pixDestroy(&image);
+	api->End();
     delete api;
-    delete [] outText;
-    pixDestroy(&image);
+	tesseract::CleanGlobalDawgCache();
 
     return 0;
 }
---
+++ testing/monolithic_tests.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#if defined(BUILD_MONOLITHIC)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int tesseract_basicAPI_test_main(int argc, const char** argv);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
